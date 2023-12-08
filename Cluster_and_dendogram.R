f29a518b72fbd0c2cbdef59e325bbbf6719e5205
berumeng <76057189+berumeng@users.noreply.github.com>
2023-12-08 09:15:01 -0700

    Update Cluster_and_dendogram.R

--- Cluster_and_dendogram.R
+++ Cluster_and_dendogram.R
@@ -4,7 +4,7 @@ library(gplots)
 library (RColorBrewer)
 
 ##Establecer la carpeta de trabajo
-setwd("/Users/berumen/Desktop/UAN/Clases/Posgrado/CBAP/2020/Análisis_visualización_datos_R/R_script/PCA_CA_clusterplot/")
+setwd("Ruta/de/trabajo")
 ##cargar el set de datos con nivles de expresión normalizados a cpm
 Table1 = read.table("Heatmap conF.txt", header=TRUE, row.names=1)
 head(Table1)
